257da189e571cdff7d9b67f8f2462cde6b2e9e75
lucas <lucas.estevao@cide.es>
2025-12-17 19:25:35 +0100

    Modificacion variable a

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ public class Main {
     
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+
+        int a = 7;
+        a++;
+        System.out.println("El nuevo valor de a es " + a);
     }
 
 
